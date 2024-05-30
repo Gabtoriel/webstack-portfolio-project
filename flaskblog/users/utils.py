897dcd39074dc1f6a20e2048c89434ebd2e8aa1e
Chibuike Eke <gabtorielvix@gmail.com>
2024-05-30 10:04:19 +0100

    pycodestyle code corrections.

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -9,9 +9,10 @@ from flaskblog import mail
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex +f_ext
-    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
-    
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics',
+                                picture_fn)
+
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
@@ -23,11 +24,12 @@ def save_picture(form_picture):
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Reset',
-                    sender='noreply@demo.com',
-                    recipients=[user.email])
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link
 {url_for('users.reset_token', token=token, _external=True)}
 
-If you did not make this request, simply ignore this email and no changes will be made
+If you did not make this request, simply ignore this email and no changes will\
+ be made
 '''
     mail.send(msg)
