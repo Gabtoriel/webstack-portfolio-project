fdd37e23cee211c355f2875a4e6291f43e9e63f9
Chibuike Eke <gabtorielvix@gmail.com>
2024-05-30 12:57:50 +0100

    changed variable name.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export FLASK_APP=run.py
-export MAIL_USERNAME=viclinstoriel@gmail.com
-export MAIL_PASSWORD="tqdv rvlj mtvg hqza"
+export EMAIL_USER=viclinstoriel@gmail.com
+export EMAIL_PASS="tqdv rvlj mtvg hqza"
